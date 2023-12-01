2f5e0ebc36d8bee47e890670d4e077e02159a088
vlcl753 <72334029+vlcl753@users.noreply.github.com>
2023-12-01 11:53:42 +0900

    변경

--- PickUp2U/Program.cs
+++ PickUp2U/Program.cs
@@ -16,7 +16,7 @@ namespace PickUp2U
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Searchform());
+            Application.Run(new ProgressSettingform());
         }
     }
 }
