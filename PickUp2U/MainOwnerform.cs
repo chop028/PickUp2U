082e4984ffd72872b1d812effa46dfe63b4235e3
JinTaekLim <jintaek019@gmail.com>
2023-12-14 13:47:14 +0900

    병합

--- PickUp2U/MainOwnerform.cs
+++ PickUp2U/MainOwnerform.cs
@@ -57,7 +57,6 @@ namespace PickUp2U
             OwnerShop.Show();
         }
 
-<<<<<<< HEAD
         private void 픽업관리ToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -75,11 +74,6 @@ namespace PickUp2U
             this.Close();
             OrderListform orderListform = new OrderListform();
             orderListform.Show();
-=======
-        private void 주문조회ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
->>>>>>> a7148f9b4085a33afe6e7cd5e4f7c13827f604cf
         }
     }
 }
